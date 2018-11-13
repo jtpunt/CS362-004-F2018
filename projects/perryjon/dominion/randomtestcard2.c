f7fe972aea03fd9b1ab02954cbd4138578c2f4fa
jtpunt <perryjon@oregonstate.edu>
2018-11-12 18:45:29 -0600

    modified randomtestcard2.c

---
+++ projects/perryjon/dominion/randomtestcard2.c
@@ -0,0 +1,35 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "unittestHelper.h"
+#include "rngs.h"
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#define TESTS 3
+int main(int argc, char** argv) {
+    srand(time(NULL));
+    struct gameState stateA, stateB;
+    int currPlayer, numPlayers;
+    int a, i, j, seed = (rand() % 1000) + 1000;
+    int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+    printf ("Testing Council card\n");
+    bool results[TESTS * 2];
+    int count = 0;
+    for(i = 0; i < TESTS; i++){
+        initializeGame(2, k, 4, &stateA);
+        stateA.handCount[stateA.whoseTurn] = rand() % MAX_HAND;
+        int cardsOnHand = numHandCards(&stateA); // cardsOnHand before calling the card's function - which will increase the # of cards on hand by 5
+        int numBuys = stateA.numBuys; // numBuys before calling the card's function - which will increase by 1 
+
+        // memcpy(&stateB, &stateA, sizeof(struct gameState));
+        a = cardEffect(smithy, 0, 0, 0, &stateA, 1, 0);
+
+        results[count++] = assertTrue("cardsOnHand", cardsOnHand + 3 - 1, numHandCards(&stateA));
+        results[count++] = assertTrue("value returned", a, 0);
+    }
+    printFinalResult(results, TESTS);
+    return 0;
+}
